b34657666713a1824701d3d8ca1ddc075e7d5f96
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-03-10 20:41:16 -0300

    init project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+    <vue-tree
+      style="width: 800px; height: 600px; border: 1px solid gray;"
+      :dataset="sampleData"
+      :config="treeConfig"
+    >
+    </vue-tree>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import VueTree from '@ssthouse/vue-tree-chart'
+import Vue from 'vue'
+Vue.component('vue-tree', VueTree)
 export default {
-  name: "App",
-  components: {
-    HelloWorld
+  name: 'treemap',
+  data() {
+    return {
+      sampleData: {
+        value: '1',
+        children: [
+          { value: '2', children: [{ value: '4' }, { value: '5' }] },
+          { value: '3' }
+        ]
+      },
+      treeConfig: { nodeWidth: 120, nodeHeight: 80, levelHeight: 200 }
+    }
   }
-};
+}
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped lang="scss">
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
