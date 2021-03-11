a4211e258df03c42b306cc8d2fa36db406befaf8
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-03-10 21:25:20 -0300

    create style

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,47 @@
 <template>
-  <div class="container">
-    <vue-tree
-      style="width: 1000px; height: 600px; border: 1px solid gray;"
-      :dataset="richMediaData"
-      :config="treeConfig"
-    >
-      <template v-slot:node="{ node, collapsed }">
-        <div
-          class="rich-media-node"
-          :style="{ border: collapsed ? '2px solid grey' : '' }"
-        >
-          <span style="padding: 4px 0; font-weight: bold;"
-            >{{ node.name }}</span
+  <div id="app">
+    <div class="container">
+      <vue-tree
+        style="width: 1000px; height: 600px;"
+        :dataset="richMediaData"
+        :config="treeConfig"
+      >
+        <template v-slot:node="{ node, collapsed }">
+          <div
+            class="rich-media-node"
+            :style="{ border: collapsed ? '2px solid grey' : '' }"
           >
-        </div>
-      </template>
-    </vue-tree>
+            <span style="padding: 4px 0; font-weight: bold;">{{
+              node.name
+            }}</span>
+          </div>
+        </template>
+      </vue-tree>
+    </div>
   </div>
 </template>
 
 <script>
+import api from "./data/organization.json";
 export default {
-  name: 'treemap',
+  name: "treemap",
   data() {
     return {
-      richMediaData: {},
-      treeConfig: { nodeWidth: 120, nodeHeight: 80, levelHeight: 200 }
-    }
-  }
-}
+      richMediaData: api,
+      treeConfig: { nodeWidth: 120, nodeHeight: 80, levelHeight: 200 },
+    };
+  },
+};
 </script>
 
 <style scoped lang="scss">
 .container {
+  margin: 0 auto;
   display: flex;
   flex-direction: column;
   align-items: center;
+  background: #14151f;
+  height: 100vh;
 }
 
 .rich-media-node {
@@ -46,7 +52,7 @@ export default {
   align-items: flex-start;
   justify-content: center;
   color: white;
-  background-color: #1b29ee;
+  background-color: #232436;
   border-radius: 4px;
 }
 </style>
